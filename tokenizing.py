0be85fe5d51c2362bc6bf84369db2a8494e242a8
Teerawat <82719459+Teerawa-ctrl@users.noreply.github.com>
2024-07-20 22:15:57 +0700

    Tokenizing
    Basic Tokenizing

---
+++ tokenizing.py
@@ -0,0 +1,15 @@
+import nltk
+
+#nltk.download()
+
+#tokenizing 
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = " Hello Mr.John , how are you doing to day ? The weather is great and python is great , The sky to day is blue?"
+
+#print(sent_tokenize(example_text)) #แยกประโยค
+#print(word_tokenize(example_text)) #แยกทีละคำ
+
+for i in word_tokenize(example_text) :
+    print(i)
